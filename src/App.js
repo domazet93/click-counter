a56b284e386bca1651098c50d42b038ca3e6b81a
Toni Domazet <tdomazet@shutterstock.com>
2019-05-31 12:17:40 +0100

    app component - first red-green test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    counter: 0
+  }
+
+  incrementCounter = () => {
+    const count = this.state.counter;
+    this.setState({
+      counter : count + 1
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <h1>Hello world</h1>
+      <div className="App" data-test="component-app">
+{/*         <h1>The count is {this.state.counter}</h1>
+        <button onClick={this.incrementCounter}>Increment counter</button> */}
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Enzyme, { shallow} from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
+
 import App from './App';
 
 Enzyme.configure(({ adapter: new EnzymeAdapter }));
 
-it('renders without crashing', () => {
+it('renders without error', () => {
   const wrapper = shallow(<App />);
-  expect(wrapper).toBeTruthy()
+  const appWrapper = wrapper.find('[data-test="component-app"]');
+  expect(appWrapper.length).toBe(1);
+});
+it('renders increment button', () => {
+
+});
+it('renders counter display', () => {
+
+});
+it('counter starts with 0', () => {
+
+});
+it('clicking button increments counter display', () => {
+
 });
